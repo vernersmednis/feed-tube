b5a1cc6442fd00280f8843735b7fe899e7c7ad3f
Verners Mednis <verners.mednis@inbox.lv>
2024-08-14 15:46:45 +0300

    Set the default route to the home view

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
